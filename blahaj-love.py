7138d6da82d1f27d29c62631b1b345a6abf650f7
lazy-af-7 <honey.batra007@icloud.com>
2021-04-01 09:46:43 +0530

    all hail blahaj

---
+++ blahaj-love.py
@@ -0,0 +1,33 @@
+print("We all love Blahaj")
+print('''                                                                                                    
+                                                      `.-:.                                         
+                                                    .:+sso:                                         
+                                                  `/ossso/                                          
+                                                 .ossssso.                                          
+                                                -ossssys+                                     .//:  
+                                               -ssssssso/                                   .+sso:  
+                                              -sssssssoo.                                 `/ssso/`  
+                                             -syssyyysoo                                 -oyyso/`   
+                                           `-ssyyyyysss+                               `/syyso+`    
+                                        `.-+syyyyyyyssoo                              .+syssso.     
+                          ``.....---:::/+++oooossyhhyyo+-                -o.         -syyysoo.      
+              `..-:://///++++++++++++++++++++++ooooooooo+/.            .+ys`     `.:+osyyyso-       
+        .-:/++++++++++oo++o+++oooooo+++oooooooooooooooooo+//////-....-/syso::-:/+sssyssyyss+-       
+     ./+oossooosooooo+ooooooooooooooooooooooooooooooossoso+/+oosssooossssso++oooosyoysshhysoo.      
+   -oossossssooossooooosoooooooooooooooossosssssssssssssssooooooshssssyyyyysoossooss+sohhyyyso-     
+  -oyyyyssssssssssssssooooooossssssssssssssssssssssssssssssssooosohsysssyssyooosoooo/--+syhhyys/    
+  /syyyyyysssssooosssssssssssssssssssysyyssssssssssssssssssossoososysyosyossooooo+/.     .:oyyys+   
+   +syyyyhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysssssssssoosssoo+ssoyoosoos++o/-`          `:++/   
+   .+ssyyyhyyhhhhhhhhhhhyyyyyyyyyyyyyyyyyyyyyyyyyysssssssssosssoo+oosoo+o+o+:-`                     
+     .-/+oyyyyyyyhhdmyhyyyyyyyyyyyyyyyyyyyyyyyyyysssssosooooosooo+oooo+/:-`                         
+       ``..----://+syyyyyyyyyyyhyyyyhyhyyyyyyyyyyssssoooooooooo++//:-.`                             
+         ````....`..-:/+ooossosssyyhyhyyyyhyyyyyyssssos+///::--``                                   
+           ``..............--------://++ohhhhhyyyyyss/-.```                                         
+               ```......--.........-.---.shhhhhyyhysso-`                                            
+                   ````............------/yhhhyhhhyyysoo:.                                          
+                        ``...----------:-::+yyyyyyyyyyssss+-.``                                     
+                                            `-/osyysssyyyyysso++/:-`                                
+                                                `./oossyyyyyyyyyyys/                                
+                                                     `.--:://///:-.                                 
+                                                                                                    ''')
+print("ALL HAIL BLAHAJ")
